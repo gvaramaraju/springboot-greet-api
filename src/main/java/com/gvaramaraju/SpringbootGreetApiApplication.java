83feb77261f890550d8ebdac404795117b5cacb5
gvaramaraju <ramaraju.gva@gmail.com>
2022-06-25 10:04:40 +0530

    Initial commit

---
+++ src/main/java/com/gvaramaraju/SpringbootGreetApiApplication.java
@@ -0,0 +1,13 @@
+package com.gvaramaraju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGreetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGreetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gvaramaraju/SpringbootGreetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gvaramaraju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGreetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
